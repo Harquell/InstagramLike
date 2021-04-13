f4c5f4221e7ddeabe46f1548cac36977f121d660
Loïc HARQUEL <lharquel@smag.tech>
2021-04-13 23:17:55 +0200

    Initial Commit

---
+++ Shared/InstagramLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramLikeApp.swift
+//  Shared
+//
+//  Created by river on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
